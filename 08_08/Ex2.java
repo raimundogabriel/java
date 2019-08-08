d3f2f5ac1d9b608dd86015491f132b115232cd70
raimundogabriel <raimundogabriel18@gmail.com>
2019-08-08 19:49:03 -0300

    Add files via upload

---
+++ 08_08/Ex2.java
@@ -0,0 +1,12 @@
+package ex2;
+
+import java.util.Scanner;
+
+public class Ex2 {
+    public static void main(String[] args) {
+        float c,f;
+        System.out.println("qual a temperatura em graus Celsius?");
+        Scanner s = new Scanner(System.in);
+    }
+    
+}
