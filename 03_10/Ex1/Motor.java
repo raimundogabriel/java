0a89e7088092b43862e590eb31be6c0d9f3ab5c5
raimundogabriel <raimundogabriel18@gmail.com>
2019-10-03 21:55:14 -0300

    Create Motor.java

---
+++ 03_10/Ex1/Motor.java
@@ -0,0 +1,15 @@
+package ex2;
+
+
+public class Motor {
+    private float cilindradas;
+    
+    public void setCilindradas(float cilindradas){
+        this.cilindradas = cilindradas;
+        
+    }
+    public float getCilindradas(){
+        return this.cilindradas;
+    }
+    
+}
